7e458a09e5ed455261260398b4800fb530514b99
ItallMakesense <kirgenvall@gmail.com>
2017-03-14 13:29:01 +0300

    done?

--- python_4_1.py
+++ python_4_1.py
@@ -1,6 +1,3 @@
-import pprint
-
-
 units = {1: 'one', 2: 'two', 3: 'three',
          4: 'four', 5: 'five', 6: 'six',
          7: 'seven', 8: 'eight', 9: 'nine'}
@@ -12,9 +9,8 @@ decades = {10: 'ten', 11: 'eleven', 12: 'twelve',
            40: 'fourty', 50: 'fifty', 60: 'sixty',
            70: 'seventy', 80: 'eighty', 90: 'ninety'}
 
-majors = {1: units, 10: decades, 100: 'hundred',
+majors = {1: 1, 10: 10, 100: 'hundred',
           1000: 'thousand', 1000000: 'million'}
-
 
 def row_making(number, num_row, majors):
   for i, major in enumerate(majors):
@@ -29,28 +25,31 @@ def row_making(number, num_row, majors):
         number = number % major
   return num_row
 
-number = 11
-
-majors_numbers = sorted(majors, reverse=True)
-numb_row = row_making(number, {}, majors_numbers)
-
-pprint.pprint(numb_row)
-
-def hundreds_of_text(numb_row, text_number):
+def int_to_text(numb_row, text_number=list(), dozen=False):
     for number, count in sorted(numb_row.items(), reverse=True):
       if type(count) == type(dict()):
-        print ("Got deeper")
-        text_number = hundreds_of_text(count, text_number)
+        text_number = int_to_text(count, text_number)
         text_number.append(majors[number])
         continue
       if number <= 10:
         if number == 10:
-          text_number.append(decades[10*count])
+          if count == 1: dozen = True
+          text_number.append(decades[10 * count])
         if number == 1:
-          text_number.append(units[count])
+          if dozen:
+            text_number.pop(len(text_number) - 1)
+            text_number.append(decades[10 + count])
+          else:
+            text_number.append(units[count])
       else:
-        print (count)
-
         text_number.append(units[count])
         text_number.append(majors[number])
     return text_number
+
+def int_to_words(number):
+    numb_row = row_making(int(number), {}, sorted(majors, reverse=True))
+    return " ".join(int_to_text(numb_row))
+
+number = input("Type integer number here: ")
+
+print (int_to_words(number))
